8e3e3d50d1eec3f16487aad19d38c11c3c997424
Folee <597171538@qq.com>
2023-09-10 20:25:53 +0800

    feat: project init

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    $schema: 'https://json.schemastore.org/prettierrc',
+    semi: false,
+    singleQuote: true,
+    printWidth: 80,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    singleAttributePerLine: true
+}
